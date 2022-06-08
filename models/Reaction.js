da3e1224d6074e90c29659f19da593f8f6ec32a0
dokodesara <dokodesara@gmail.com>
2022-06-07 21:35:40 -0400

    lastfix

---
+++ models/Reaction.js
@@ -0,0 +1,32 @@
+const { Schema, Types } = require('mongoose');
+const moment = require('moment');
+
+// reaction schema tied to thought
+const reactionSchema = new Schema({
+    // set custom id to avoid confusion with parent id
+    reactionId: {
+        type: Schema.Types.ObjectId,
+        default: () => new Types.ObjectId()
+    },
+    reactionBody: {
+        type: String,
+        required: true,
+        maxlength: 280
+    },
+    username: {
+        type: String,
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+        get: createdAtVal => moment(createdAtVal).format('MMM DD, YYYY [at] hh:mm:a')
+    }
+}, {
+    toJSON: {
+        getters: true
+    },
+    id: false
+});
+
+module.exports = reactionSchema;
---
+++ models/Thought.js
@@ -0,0 +1,40 @@
+const { Schema, model } = require('mongoose');
+const moment = require('moment');
+const reactionSchema = require('./Reaction');
+
+// thought schema
+const thoughtSchema = new Schema({
+    thoughtText: {
+        type: String,
+        required: true,
+        maxlength: 280
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+        get: (createdAtVal) => moment(createdAtVal).format('MMM DD, YYYY [at] hh:mm a')
+    },
+    username: {
+        type: String,
+        required: true
+    },
+    // tying reactions to thought
+    reactions: [reactionSchema]
+}, {
+    toJSON: {
+        virtuals: true,
+        getters: true
+    },
+    id: false
+});
+
+// get total count of friends
+thoughtSchema.virtual('reactionCount').get(function() {
+    return this.reactions.length;
+});
+
+// create the User model using the UserSchema
+const Thought = model('Thought', thoughtSchema);
+
+// export the User model
+module.exports = { Thought };
